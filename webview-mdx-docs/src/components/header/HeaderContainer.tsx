c03ae237216d78dde42aad9ea4d7c294ae4e27bf
zafkiel45 <zafkielc@gmail.com>
2025-04-08 16:22:10 -0300

    feat: header added

---
+++ webview-mdx-docs/src/components/header/HeaderContainer.tsx
@@ -0,0 +1,16 @@
+export const HeaderContainer = () => {
+  return (
+    <header className="h-[5vh]">
+        <nav>
+            <ul className="flex gap-2">
+                <li>
+                    Tema
+                </li>
+                <li>
+                    Ajuda
+                </li>
+            </ul>
+        </nav>
+    </header>
+  );
+};
