8498f75653df4e22911a5cbbb180e00ee69000d4
zafkiel45 <zafkielc@gmail.com>
2025-04-09 15:13:45 -0300

    feat: modified header feature

--- webview-mdx-docs/src/components/header/HeaderContainer.tsx
+++ webview-mdx-docs/src/components/header/HeaderContainer.tsx
@@ -3,11 +3,8 @@ export const HeaderContainer = () => {
     <header className="h-[5vh] my-2">
         <nav>
             <ul className="flex gap-2">
-                <li>
-                    Tema
-                </li>
-                <li>
-                    Ajuda
+                <li className="bg-green-600 text-white rounded-md py-1 px-2">
+                    Novidades em breve!
                 </li>
             </ul>
         </nav>
