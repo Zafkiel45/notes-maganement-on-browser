05c558f84c6bd08a06e7a8b6628f7b3f7abc3b41
zafkiel45 <zafkielc@gmail.com>
2025-04-17 14:53:43 -0300

    refactor: turned navbar react component into astro component

--- src/components/navbar/Button.navbar.tsx
+++ src/components/navbar/Button.navbar.tsx
@@ -1,18 +1,20 @@
 export const NavbarBtn = ({
-    content,
-    svg,
-    ...props
-}:{content: string, svg: React.JSX.Element}) => {
-    return (
-        <div {...props} className={`bg-[#1B1B1B] lg:px-3 lg:py-2 w-full border border-[#464646] rounded-lg shadow-sm cursor-pointer hover:bg-[#242424] active:bg-[#2e2e2e] transition-colors`}>
-            <button className="flex gap-2 items-center">
-                <div>
-                    {svg}
-                </div>
-                <div>
-                    {content}
-                </div>
-            </button>
-        </div>
-    )
-}
+  content,
+  children,
+  ...props
+}: {
+  content: string;
+  children: React.JSX.Element;
+}) => {
+  return (
+    <div
+      {...props}
+      className={`bg-[#1B1B1B] lg:px-3 lg:py-2 w-full border border-[#464646] rounded-lg shadow-sm cursor-pointer hover:bg-[#242424] active:bg-[#2e2e2e] transition-colors`}
+    >
+      <button className="flex gap-2 items-center">
+        <div>{children}</div>
+        <div>{content}</div>
+      </button>
+    </div>
+  );
+};
