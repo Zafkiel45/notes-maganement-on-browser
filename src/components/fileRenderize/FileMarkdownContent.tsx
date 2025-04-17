6dc8e7dbe54a5bb4f8e20a032050b81b53598fdb
zafkiel45 <zafkielc@gmail.com>
2025-04-17 10:56:59 -0300

    feat: highlight code added

--- src/components/fileRenderize/FileMarkdownContent.tsx
+++ src/components/fileRenderize/FileMarkdownContent.tsx
@@ -1,21 +1,17 @@
-import React from 'react';
-import Markdoc from '@markdoc/markdoc';
+import React from "react";
+import Markdoc from "@markdoc/markdoc";
 
-export function FileContent({children}:{children: string}) {
-    return (
-        <>
-            {children}
-        </>
-    )
-};
+export function FileContent({ children }: { children: React.JSX.Element[] }) {
+  return <>{children}</>;
+}
 
-export function ParseFileContent({markdown}:{markdown: string}) {
-    const parsedContent = Markdoc.parse(markdown);
-    const transformedContent = Markdoc.transform(parsedContent);
-    
-    return Markdoc.renderers.react(transformedContent, React, {
-        components: {
-          FileContent: FileContent
-        }
-      });
-};
+export function ParseFileContent({ markdown }: { markdown: string }) {
+  const parsedContent = Markdoc.parse(markdown);
+  const transformedContent = Markdoc.transform(parsedContent);
+
+  return Markdoc.renderers.react(transformedContent, React, {
+    components: {
+      FileContent: FileContent,
+    },
+  });
+}
