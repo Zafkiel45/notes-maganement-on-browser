7de1c8b84da09e2a0c8ee49998e1974160a0ea2a
zafkiel45 <zafkielc@gmail.com>
2025-05-02 10:10:14 -0300

    feat(navbar):Reactivity added

--- src/scripts/navbar/modal/folderModal.ts
+++ src/scripts/navbar/modal/folderModal.ts
@@ -5,6 +5,7 @@ import type {
   InputElement,
 } from "../types/htmlElements";
 import { addClass, removeClass, toggleModal } from "../utils/utlity.navbar";
+import { createFolderNode } from "../folders/folderList";
 
 // related to generics  elements
 const modalError = document.querySelector("#modalError") as DivElement;
@@ -48,18 +49,6 @@ const createFolder = async () => {
   try {
     const folderName = capitalizeFirstLetter(folderNameInput.value);
 
-    if (folderName === "") {
-      setTimeout(() => {
-        removeClass(modalError, "flex");
-        addClass(modalError, "hidden");
-      }, 3000);
-
-      removeClass(modalError, "hidden");
-      addClass(modalError, "flex");
-      modalErrorMessage.textContent = "Nome da pasta vazia";
-      return;
-    }
-
     await fetch("http://localhost:3001/folders", {
       method: "POST",
       mode: "cors",
@@ -68,38 +57,11 @@ const createFolder = async () => {
       },
       body: JSON.stringify({ folderName: folderName }),
     }).then((answer) => {
-      if (answer.status === 409) {
-        // there is a possbility of extract this into util function
-        setTimeout(() => {
-          removeClass(modalError, "flex");
-          addClass(modalError, "hidden");
-        }, 3000);
-
-        removeClass(modalError, "hidden");
-        addClass(modalError, "flex");
-        modalErrorMessage.textContent = "Nome da Pasta já existe";
-
-        return;
-      } else if (answer.status === 400) {
-        setTimeout(() => {
-          removeClass(modalError, "flex");
-          addClass(modalError, "hidden");
-        }, 3000);
-
-        removeClass(modalError, "hidden");
-        addClass(modalError, "flex");
-        modalErrorMessage.textContent = "Nome da pasta vazia!";
-      } else {
-        setTimeout(() => {
-          removeClass(modalSuccess, "flex");
-          addClass(modalSuccess, "hidden");
-        }, 3000);
-
-        removeClass(modalSuccess, "hidden");
-        addClass(modalSuccess, "flex");
-        modalSuccessMessage.textContent = "Pasta criada com sucesso!";
-      }
+      if (answer.status === 409) {return} 
+      createFolderNode(folderName); 
     });
+
+    return;
   } catch (err) {
     console.error(err);
   }
