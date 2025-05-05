0c67e9534c3f311f3387e7b887d0834c7566f522
zafkiel45 <zafkielc@gmail.com>
2025-05-05 11:21:00 -0300

    fix: reactivity on note options

--- src/scripts/navbar/modal/noteModal.ts
+++ src/scripts/navbar/modal/noteModal.ts
@@ -1,25 +1,13 @@
-import { deleteNoteEvent, editNoteEvent } from "../../notesOptions.ts";
+import { deleteNode, editNode } from "../utils/utlity.navbar";
 import { toggleModal } from "../utils/utlity.navbar";
-const openModalBtn = document.querySelector("#openModal") as HTMLButtonElement;
-const modal = document.querySelector("#modal") as HTMLDivElement;
-const closeModalBtn = document.querySelector("#closeModal") as HTMLButtonElement;
-const createNoteBtn = document.querySelector("#createNote") as HTMLButtonElement;
-const fileList = document.querySelector('#fileList') as HTMLUListElement;
-
-openModalBtn.addEventListener("click", () => {
-  toggleModal(true, modal);
-});
-
-closeModalBtn.addEventListener("click", () => {
-  toggleModal(false, modal);
-});
 
 document.addEventListener('astro:page-load', () => {
+  const fileList = document.querySelector('#fileList') as HTMLUListElement;
   const openModalBtn = document.querySelector("#openModal") as HTMLButtonElement;
   const modal = document.querySelector("#modal") as HTMLDivElement;
   const closeModalBtn = document.querySelector("#closeModal") as HTMLButtonElement;
   const createNoteBtn = document.querySelector("#createNote") as HTMLButtonElement;
-
+  
   closeModalBtn.addEventListener("click", () => {
     toggleModal(false, modal);
   });
@@ -28,71 +16,65 @@ document.addEventListener('astro:page-load', () => {
     toggleModal(true, modal);
   });
 
-  createNoteBtn.addEventListener("click", () => {
-     createNote();
+  createNoteBtn.addEventListener("click", async () => {
+     await createNote();
   });
-});
 
-createNoteBtn.addEventListener("click", async () => {
-  await createNote();
-});
-
-async function createNote() {
-  const noteName = (document.querySelector("#noteName") as HTMLInputElement).value;
-  const selectedFolder = document.querySelector('input[name="folder"]:checked');
-  const noteFolder = (selectedFolder as HTMLInputElement).value; 
-  const noteContent = (document.querySelector("#noteEditor") as HTMLTextAreaElement).value;
-
-  try {
-    console.log('called')
-    await fetch("http://localhost:3001/note", {
-      method: "POST",
-      mode: "cors",
-      body: JSON.stringify({
-        name: noteName,
-        content: noteContent,
-        folder: parseInt(noteFolder),
-      }),
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
-
-    const targetChild = fileList.lastElementChild;
-
-    if(targetChild === null) {
-      window.location.reload();
-      return;
+  async function createNote() {
+    const noteName = (document.querySelector("#noteName") as HTMLInputElement).value;
+    const selectedFolder = document.querySelector('input[name="folder"]:checked');
+    const noteFolder = (selectedFolder as HTMLInputElement).value; 
+    const noteContent = (document.querySelector("#noteEditor") as HTMLTextAreaElement).value;
+  
+    try {
+      await fetch("http://localhost:3001/note", {
+        method: "POST",
+        mode: "cors",
+        body: JSON.stringify({
+          name: noteName,
+          content: noteContent,
+          folder: parseInt(noteFolder),
+        }),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+  
+      const targetChild = fileList.lastElementChild;
+  
+      if(targetChild === null) {
+        window.location.reload();
+        return;
+      };
+  
+      const targetChildId = parseInt(targetChild.getAttribute('data-id') as string);
+      const copyOfTarget = targetChild.cloneNode(true) as HTMLElement;
+      const copyId = String(targetChildId + 1);
+      const linkToEndpoint = copyOfTarget.querySelector('a') as HTMLAnchorElement;
+      const copyName = linkToEndpoint.querySelector('div') as HTMLDivElement;
+      const copyEditSvg = copyOfTarget.querySelector('.edit-note') as SVGElement;
+      const copyDeleteSvg = copyOfTarget.querySelector('.delete-note') as SVGElement;
+  
+      copyOfTarget.removeAttribute('data-id');
+      copyOfTarget.setAttribute('data-id', copyId);
+  
+      copyEditSvg.removeAttribute('data-id');
+      copyEditSvg.setAttribute('data-id', copyId);
+      
+      copyDeleteSvg.removeAttribute('data-id');
+      copyDeleteSvg.setAttribute('data-id', copyId);
+  
+      linkToEndpoint.removeAttribute('href');
+      linkToEndpoint.setAttribute('href', `/docs/${copyId}/files`); 
+  
+      deleteNode(copyDeleteSvg);
+      editNode(copyEditSvg);
+  
+      copyName.textContent = noteName;
+  
+      fileList.appendChild(copyOfTarget);
+    } catch (err) {
+      console.error(err);
     };
-
-    const targetChildId = parseInt(targetChild.getAttribute('data-id') as string);
-    const copyOfTarget = targetChild.cloneNode(true) as HTMLElement;
-    const copyId = String(targetChildId + 1);
-    const linkToEndpoint = copyOfTarget.querySelector('a') as HTMLAnchorElement;
-    const copyName = linkToEndpoint.querySelector('div') as HTMLDivElement;
-    const copyEditSvg = copyOfTarget.querySelector('.edit-note') as SVGElement;
-    const copyDeleteSvg = copyOfTarget.querySelector('.delete-note') as SVGElement;
-
-    copyOfTarget.removeAttribute('data-id');
-    copyOfTarget.setAttribute('data-id', copyId);
-
-    copyEditSvg.removeAttribute('data-id');
-    copyEditSvg.setAttribute('data-id', copyId);
-    
-    copyDeleteSvg.removeAttribute('data-id');
-    copyDeleteSvg.setAttribute('data-id', copyId);
-
-    linkToEndpoint.removeAttribute('href');
-    linkToEndpoint.setAttribute('href', `/docs/${copyId}/files`); 
-
-
-    deleteNoteEvent(copyDeleteSvg);
-    editNoteEvent(copyEditSvg);
-
-    copyName.textContent = noteName;
-
-    fileList.appendChild(copyOfTarget);
-  } catch (err) {
-    console.error(err);
-  };
-}
+  }
+});
--- src/scripts/navbar/utils/utlity.navbar.ts
+++ src/scripts/navbar/utils/utlity.navbar.ts
@@ -1,4 +1,4 @@
-import type { DivElement } from "../types/htmlElements";
+import type { DivElement, InputElement, UListElement } from "../types/htmlElements";
 
 // utility function to nav
 export function toggleModal(state: boolean, element: DivElement) {
@@ -18,4 +18,66 @@ export function removeClass(target: HTMLElement, ...targetClass: string[]) {
 
 export function addClass(target: HTMLElement, ...targetClass: string[]) {
   target.classList.add(...targetClass);
+};
+
+export async function deleteNode(node: HTMLElement | SVGElement) {
+  node.addEventListener("click", async () => {
+    const id = node.getAttribute("data-id");
+
+    if (typeof id !== "string") return;
+
+    const res = await fetch(`http://localhost:3001/note/notes/${id}`, {
+      method: "DELETE",
+      mode: "cors",
+    });
+
+    const fetchStatus = res.status;
+    if(fetchStatus == 400 || fetchStatus === 500) return; 
+
+    const fileList = document.querySelector("#fileList") as HTMLUListElement;
+    const fileArr = Array.from(fileList.children);
+  
+    for (let file of fileArr) {
+      const elementId: string | null = file.getAttribute("data-id");
+  
+      if (elementId === null) {continue}
+      else if (elementId !== id) {continue}
+      else {file.remove()};
+    }
+  });
+};
+
+export async function editNode(node: HTMLElement | SVGElement) {
+  node.addEventListener("click", async () => {
+    const id = node.getAttribute("data-id");
+
+    if (typeof id === "undefined") {return}
+    // FIX: Changes the #editInput to .editInput. Each input must have its own
+    // state 
+    const inputEditor = document.querySelector("#editInput") as HTMLTextAreaElement;
+    const confirmBtn = document.querySelector("#editConfirm") as HTMLButtonElement;
+    const modal = document.querySelector("#editModal") as HTMLDivElement;
+
+    toggleModal(true, modal);
+  
+    const note = await fetch(`http://localhost:3001/note/notes/${id}`);
+    const noteContent = await note.text();
+  
+    inputEditor.value = noteContent;
+
+    confirmBtn.addEventListener("click", updateNote);
+    
+    async function updateNote() {
+      try {
+        await fetch(`http://localhost:3001/note/update`, {
+          method: "PUT",
+          mode: "cors",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ id: id, content: inputEditor.value }),
+        }).then(() => {confirmBtn.removeEventListener('click', updateNote)});
+      } catch (err) {console.error(err)}
+    }
+  });
 };
