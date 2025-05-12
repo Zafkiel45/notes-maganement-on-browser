e7464fe3efeccdf7106a1bc5c40f3eab4fa782c2
zafkiel45 <zafkielc@gmail.com>
2025-05-12 10:24:21 -0300

    feat: added enter action for editModals

--- src/scripts/navbar/modal/events/keyboard.navbar.ts
+++ src/scripts/navbar/modal/events/keyboard.navbar.ts
@@ -1,4 +1,4 @@
-import type { DivElement } from "../../types/htmlElements";
+import type { BtnElement, DivElement } from "../../types/htmlElements";
 import { addClass, removeClass } from "../../utils/utlity.navbar";
 const modals = document.querySelectorAll(".modals") as NodeListOf<DivElement>;
 
@@ -15,6 +15,41 @@ document.addEventListener('astro:page-load', () => {
         };
       });
     });
+
+    modals.forEach((modal) => {
+      modal.addEventListener('keydown', (key: KeyboardEvent) => {
+        if(key.key === 'Enter') {
+          try {
+            const confirmBtn = modal.querySelector('.confirm-btn') as BtnElement;
+            confirmBtn.click();
+
+            const successAdvice = modal.querySelector('.success-advice') as DivElement;
+            successAdvice.style.display = 'flex';
+
+            const successMessage = successAdvice.querySelector('.success-message') as DivElement;
+            successMessage.textContent = 'O conteúdo foi editado com successo!'
+
+            setTimeout(() => {
+              successAdvice.style.display = 'none';
+              successMessage.textContent = '';
+            }, 3000);
+            return; 
+          } catch {
+            const errorAdvice = modal.querySelector('.error-advice') as DivElement;
+            errorAdvice.style.display = 'flex';
+
+            const errorMessage = errorAdvice.querySelector('.error-message') as DivElement;
+            errorMessage.textContent = 'Error desconhecido. A nota não foi editada';
+
+            setTimeout(() => {
+              errorAdvice.style.display = 'none';
+              errorMessage.textContent = '';
+            }, 3000);
+            return;
+          };
+        };
+      });
+    }); 
   };
 
   attachKeyEvents();
