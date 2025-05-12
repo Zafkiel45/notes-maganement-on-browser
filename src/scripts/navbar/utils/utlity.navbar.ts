e9747351e18896c08236b8dff8290345f2654138
zafkiel45 <zafkielc@gmail.com>
2025-05-12 18:10:45 -0300

    fix(editModal): wrong update of notes fixed

--- src/scripts/navbar/utils/utlity.navbar.ts
+++ src/scripts/navbar/utils/utlity.navbar.ts
@@ -1,24 +1,29 @@
-import type { DivElement, InputElement, UListElement } from "../types/htmlElements";
+import type {
+  BtnElement,
+  DivElement,
+  InputElement,
+  UListElement,
+} from "../types/htmlElements";
 
 // utility function to nav
 export function toggleModal(state: boolean, element: DivElement) {
   if (state) {
-    removeClass(element, 'hidden');
-    addClass(element, 'flex');
+    removeClass(element, "hidden");
+    addClass(element, "flex");
     element.focus();
   } else {
-    removeClass(element, 'flex');
-    addClass(element, 'hidden');
-  };
-};
+    removeClass(element, "flex");
+    addClass(element, "hidden");
+  }
+}
 
 export function removeClass(target: HTMLElement, ...targetClass: string[]) {
   target.classList.remove(...targetClass);
-};
+}
 
 export function addClass(target: HTMLElement, ...targetClass: string[]) {
   target.classList.add(...targetClass);
-};
+}
 
 export async function deleteNode(node: HTMLElement | SVGElement) {
   node.addEventListener("click", async () => {
@@ -32,52 +37,47 @@ export async function deleteNode(node: HTMLElement | SVGElement) {
     });
 
     const fetchStatus = res.status;
-    if(fetchStatus == 400 || fetchStatus === 500) return; 
+    if (fetchStatus == 400 || fetchStatus === 500) return;
 
     const fileList = document.querySelector("#fileList") as HTMLUListElement;
     const fileArr = Array.from(fileList.children);
-  
+
     for (let file of fileArr) {
       const elementId: string | null = file.getAttribute("data-id");
-  
-      if (elementId === null) {continue}
-      else if (elementId !== id) {continue}
-      else {file.remove()};
+
+      if (elementId === null) {
+        continue;
+      } else if (elementId !== id) {
+        continue;
+      } else {
+        file.remove();
+      }
     }
   });
-};
+}
 
-export async function editNode(node: HTMLElement | SVGElement) {
+export async function editNode(
+  node: HTMLElement | SVGElement,
+  modal: HTMLDivElement,
+  inputEditor: HTMLTextAreaElement,
+) {
   node.addEventListener("click", async () => {
-    const id = node.getAttribute("data-id");
+    try {
+      const id = node.getAttribute("data-id");
 
-    if (typeof id === "undefined") {return}
-    // FIX: Changes the #editInput to .editInput. Each input must have its own
-    // state 
-    const inputEditor = document.querySelector("#editInput") as HTMLTextAreaElement;
-    const confirmBtn = document.querySelector("#editConfirm") as HTMLButtonElement;
-    const modal = document.querySelector("#editModal") as HTMLDivElement;
+      if (!id) return;
 
-    toggleModal(true, modal);
-  
-    const note = await fetch(`http://localhost:3001/note/notes/${id}`);
-    const noteContent = await note.text();
-  
-    inputEditor.value = noteContent;
+      modal.removeAttribute('data-note-id');
+      modal.setAttribute('data-note-id', id);
+      
+      toggleModal(true, modal);
 
-    confirmBtn.addEventListener("click", updateNote);
-    
-    async function updateNote() {
-      try {
-        await fetch(`http://localhost:3001/note/update`, {
-          method: "PUT",
-          mode: "cors",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({ id: id, content: inputEditor.value }),
-        }).then(() => {confirmBtn.removeEventListener('click', updateNote)});
-      } catch (err) {console.error(err)}
-    }
+      const note = await fetch(`http://localhost:3001/note/notes/${id}`);
+      const noteContent = await note.text();
+
+      inputEditor.value = noteContent;
+    } catch {
+      console.error('something wrong occurred to open the edit modal');
+    };
   });
-};
+}
